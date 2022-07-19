9fa9e7285f34d6ddcbeb45243efb68bc94570189
alexander <tyjbox@gmail.com>
2022-07-20 01:00:55 +0700

    addet seminar 2

---
+++ seminar 2.py
@@ -0,0 +1,56 @@
+""" Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр"""
+
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = float(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def sumNums(num):
+    sum = 0
+    for i in str(num):
+        if i != ".":
+            sum += int(i)
+    return sum
+
+
+num = InputNumbers("Введите число: ")
+
+print(f"Сумма цифр = {sumNums(num)}")
+
+
+
+""" Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N. """
+
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = InputNumbers("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
