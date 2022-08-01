d62c703f39b41241d6090888ca89082019644ec0
alexander <tyjbox@gmail.com>
2022-08-01 22:40:41 +0700

    addet seminar 4

--- seminar 3.py
+++ seminar 3.py
@@ -133,27 +133,27 @@
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 
 
-import os
-clear = lambda: os.system('cls')
-clear()
-import random
+# import os
+# clear = lambda: os.system('cls')
+# clear()
+# import random
 
-def get_fibonacci_numbers(param_1: int):
-    fibinacci_list = []
-    fib_num_1 = 1
-    fib_num_2 = 1
-    for i in range(param_1):
-        fibinacci_list.append(fib_num_1)
-        fib_num_1, fib_num_2 = fib_num_2, fib_num_1 + fib_num_2
+# def get_fib_num(param_1: int):
+#     fib_list = []
+#     fib_num_1 = 1
+#     fib_num_2 = 1
+#     for i in range(param_1):
+#         fib_list.append(fib_num_1)
+#         fib_num_1, fib_num_2 = fib_num_2, fib_num_1 + fib_num_2
 
-    fib_num_1 = 0
-    fib_num_2 = 1
-    for i in range(param_1 + 1):
-        fibinacci_list.insert(0,fib_num_1)
-        fib_num_1, fib_num_2 = fib_num_2, fib_num_1 - fib_num_2        
+#     fib_num_1 = 0
+#     fib_num_2 = 1
+#     for i in range(param_1 + 1):
+#         fib_list.insert(0,fib_num_1)
+#         fib_num_1, fib_num_2 = fib_num_2, fib_num_1 - fib_num_2        
        
-    return fibinacci_list
+#     return fib_list
 
-number = int(input('Введите число: '))
-final_list = get_fibonacci_numbers(number)
-print(f'Cписок чисел Фибоначчи: {final_list}')
+# num = int(input('Введите число: '))
+# final_list = get_fib_num(num)
+# print(f'Cписок чисел Фибоначчи: {final_list}')
--- seminar 4.py
+++ seminar 4.py
@@ -1,40 +1,97 @@
-#Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# 1.Вычислить число c заданной точностью d
+# Пример:
+# - при d = 0.001, π = 3.141.    10^{-1} ≤ d ≤10^{-10}
 
-#Пример:
 
-#- [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+# import os
+# clear = lambda: os.system('cls')
+# clear()
+# import math
+# from math import pi
+
+# def get_count(number: float):#  определяем количество знаков после запятой
+#     number_conversion = str(number)
+#     if '.' in number_conversion:
+#         return abs(number_conversion.find('.') - len(number_conversion)) - 1
+#     else:
+#         return 0
+
+# num_pi = pi
+# print(f'Число Pi: {pi}')
+# giv_accuracy = input('Введите точность, которую нужно определить, по образцу 0.001: ')
+# accuracy_count = get_count(giv_accuracy)
+# print(f'Число Pi c заданной точностью {giv_accuracy}: {round(math.pi, accuracy_count)}')
+
+#2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# import os
+# clear = lambda: os.system('cls')
+# clear()
+
+# num = int(input('Введите число: '))
+# prime_fact_list = []
+# fact = 2
+# while num > 1:
+#     if num % fact == 0:
+#         prime_fact_list.append(fact)
+#         num = num / fact
+#     else:
+#         fact += 1
+# print(prime_fact_list)
+
+#3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+# import os
+# clear = lambda: os.system('cls')
+# clear()
+# import random
+
+
+# def fill_list(arg_1: int):
+#     new_list = []
+#     for index in range(arg_1):
+#         new_list.append(random.randint(1, 6))
+#     return new_list
+
+
+# def non_repeating_elements(arg_1: list):
+#     result_list = []
+#     for i in arg_1:
+#         if i not in result_list:
+#             result_list.append(i)
+#     return result_list
+
+# num = int(input('Задайте длину списка: '))
+# filled_list = fill_list(num)
+# print(f'Исходный список: {filled_list}')
+# final_result = non_repeating_elements(filled_list)
+# print(f'Cписок неповторяющихся элементов: {final_result}')
+
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
 import os
 clear = lambda: os.system('cls')
 clear()
 import random
+deg_of_polynom = int(input("Введите степень: "))
 
-def fill_list(arg_1: int):
-    new_list = []
-    for i in range(arg_1):
-        new_list.append(round(random.uniform(0,5), 2))
-    return new_list
+def get_polynom(param_1: int):
+    polynom = ""
+    while param_1 >= 0:
+        coeff = random.randint(0, 101)
+        if coeff != 0:
+            match param_1:
+                case 1: polynom += (str(coeff) + "*x" + " + ")
+                case 0: polynom += (str(coeff) + " = 0")
+                case _: polynom += (str(coeff) + "*x^" + str(param_1) + " + ")
+        param_1 -= 1
+    return polynom
 
+polynom_1 = get_polynom(deg_of_polynom)
+print(f'Многочлен степени {deg_of_polynom}: {polynom_1}')
 
-def get_difference(arg_1: list):
-    fractional_part = 0
-    fractional_part_list = []
-    result_of_difference = 0
-    for i in arg_1:
-        fractional_part = round(i - int(i), 2)
-        fractional_part_list.append(fractional_part)
-    print(f'Список дробых частей элементов: {fractional_part_list}')
-    
-    max(fractional_part_list)
-    print(f'Максимальная дробная часть: {max(fractional_part_list)}')
-    min(fractional_part_list)
-    print(f'Минимальная дробная часть: {min(fractional_part_list)}')
-
-    result_of_difference = round(max(fractional_part_list) - min(fractional_part_list), 2)
-    return result_of_difference
-
-number = int(input('Задайте длину списка: '))
-filled_list = fill_list(number)
-print(f'Исходный список: {filled_list}')
-final_result = get_difference(filled_list)
-print(f'Разница между максимальным и минимальным значением дробной части элементов: {final_result}')
+data = open('file_task_4.txt', 'w')
+data.write(polynom_1)
+data.close()
